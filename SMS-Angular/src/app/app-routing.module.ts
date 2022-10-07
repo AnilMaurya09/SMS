f75f0dc15420fe52628db5038e40a145b7388fd7
Aaditya Goel <aadityagoel018@gmail.com>
2022-10-07 06:16:42 +0530

    minor bootstrap changes

--- SMS-Angular/src/app/app-routing.module.ts
+++ SMS-Angular/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 const routes: Routes = [
+  { path: 'index', component: IndexComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'header', component: HeaderComponent },
+  { path: '',   redirectTo: '/index', pathMatch: 'full' },
 ];
 
 @NgModule({
